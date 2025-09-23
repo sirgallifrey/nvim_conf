9b7b89f36547248cb42fc0d1ab2db8313616e401
Adilson Schmitt Junior <adilsonschj@gmail.com>
2025-09-23 15:48:20 +0200

    Fix lspconfig deprecated warning

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,7 @@
 -- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
 -- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 local servers = {
@@ -12,17 +9,17 @@ local servers = {
   cssls = {},
   ts_ls = {},
   elixirls = {
-    cmd = { "elixir-ls" }
+    cmd = { "elixir-ls" },
   },
   tailwindcss = {
     init_options = {
       userLanguages = {
         elixir = "html-eex",
         eelixir = "html-eex",
-        heex = "html-eex"
-      }
-    }
-  }
+        heex = "html-eex",
+      },
+    },
+  },
 }
 
 for name, opts in pairs(servers) do
@@ -30,5 +27,6 @@ for name, opts in pairs(servers) do
   opts.on_attach = nvlsp.on_attach
   opts.capabilities = nvlsp.capabilities
 
-  require("lspconfig")[name].setup(opts)
+  vim.lsp.config[name] = opts
+  vim.lsp.enable(name)
 end
